470b6024afaeb000d5ef82a59430e0118b8e516e
Jacobo Rodicio González <jacobo.rodicio@it-manufactory.com>
2024-11-13 09:17:40 +0100

    feat: add port check & devSever helper

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,19 +2,46 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.config.common");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const detectPort = require("detect-port");
+const inquirer = require("inquirer");
 
-module.exports = merge(common, {
-  mode: "development",
-  entry: "./demo/index.jsx",
-  devtool: "eval-source-map",
-  devServer: {
-    static: path.resolve(__dirname, "demo"),
-    compress: true,
-    port: 3010,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./demo/index.html",
-    }),
-  ],
-});
+const DEFAULT_PORT = 3000;
+
+const getPort = async () => {
+  const port = await detectPort(DEFAULT_PORT);
+  if (port === DEFAULT_PORT) {
+    return port;
+  }
+  // If the Default Port is not available, ask the user if they want to use the next available port
+  const { useNextPort } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "useNextPort",
+      message: `Port ${DEFAULT_PORT} is already in use. Would you like to run the app on port ${port}?`,
+    },
+  ]);
+  return useNextPort ? port : null;
+};
+
+module.exports = async () => {
+  const port = await getPort();
+  if (!port) {
+    console.log("Server Cancelled. Exiting...");
+    process.exit(0);
+  }
+  return merge(common, {
+    mode: "development",
+    entry: "./demo/index.jsx",
+    devtool: "eval-source-map",
+    devServer: {
+      static: path.resolve(__dirname, "demo"),
+      compress: true,
+      port,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./demo/index.html",
+      }),
+    ],
+  });
+};
